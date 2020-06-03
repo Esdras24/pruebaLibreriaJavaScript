fa14328ee16b623b5fd251e00773064239da9823
Esdras24 <50970631+Esdras24@users.noreply.github.com>
2020-06-02 20:39:13 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ function grey(){
   $(".main-titulo").css("color","grey")
 }
 
+var score=0;
+
 //funcion para rellenar todos los espacios
 function reinsertar() {
   var child1=$(".col-1").children();
@@ -23,31 +25,31 @@ function reinsertar() {
   var child7=$(".col-7").children();
   if (child1.length<=6) {
     var x1 = Math.floor((Math.random() * 4) + 1);
-    $(".col-1").prepend("<div class='"+x1+"'><img src='image/"+x1+".png' class='img fruta'/></div>");
+    $(".col-1").prepend("<img src='image/"+x1+".png' class='fruta ui-draggable ui-droppable ui-draggable-handle' style='position: relative;'/>");
   };
   if (child2.length<=6) {
     var x2 = Math.floor((Math.random() * 4) + 1);
-    $(".col-2").prepend("<div class='"+x2+"'><img src='image/"+x2+".png' class='img fruta'/></div>");
+    $(".col-2").prepend("<img src='image/"+x2+".png' class='fruta ui-draggable ui-droppable ui-draggable-handle' style='position: relative;'/>");
   };
   if (child3.length<=6) {
     var x3 = Math.floor((Math.random() * 4) + 1);
-    $(".col-3").prepend("<div class='"+x3+"'><img src='image/"+x3+".png' class='img fruta'/></div>");
+    $(".col-3").prepend("<img src='image/"+x3+".png' class='fruta ui-draggable ui-droppable ui-draggable-handle' style='position: relative;'/>");
   };
   if (child4.length<=6) {
     var x4 = Math.floor((Math.random() * 4) + 1);
-    $(".col-4").prepend("<div class='"+x4+"'><img src='image/"+x4+".png' class='img fruta'/></div>");
+    $(".col-4").prepend("<img src='image/"+x4+".png' class='fruta ui-draggable ui-droppable ui-draggable-handle' style='position: relative;'/>");
   };
   if (child5.length<=6) {
     var x5 = Math.floor((Math.random() * 4) + 1);
-    $(".col-5").prepend("<div class='"+x5+"'><img src='image/"+x5+".png' class='img fruta'/></div>");
+    $(".col-5").prepend("<img src='image/"+x5+".png' class='fruta ui-draggable ui-droppable ui-draggable-handle' style='position: relative;'/>");
   };
   if (child6.length<=6) {
     var x6 = Math.floor((Math.random() * 4) + 1);
-    $(".col-6").prepend("<div class='"+x6+"'><img src='image/"+x6+".png' class='img fruta'/></div>");
+    $(".col-6").prepend("<img src='image/"+x6+".png' class='fruta ui-draggable ui-droppable ui-draggable-handle' style='position: relative;'/>");
   };
   if (child7.length<=6) {
     var x7 = Math.floor((Math.random() * 4) + 1);
-    $(".col-7").prepend("<div class='"+x7+"'><img src='image/"+x7+".png' class='img fruta'/></div>");
+    $(".col-7").prepend("<img src='image/"+x7+".png' class='fruta ui-draggable ui-droppable ui-draggable-handle' style='position: relative;'/>");
   };
 }
 
@@ -58,9 +60,11 @@ var temporizador=0;
 //boton de reiniciar
 $(function() {
   $(".btn-reinicio").click(function () {
+    $('.fruta').css("justify-content");
     clearInterval(temporizador);
     $(".panel-score").css("width","25%");
   	$(".panel-tablero").show();
+    score=0;
     var a=1;
     while (a<=7) {
       $(".col-"+a+"").empty();
@@ -72,6 +76,7 @@ $(function() {
     $(this).text("Reiniciar");
     minuto=2;
     segundo=0;
+    $("#score-text").html(" "+score+" ");
   });
 })
 
@@ -99,100 +104,160 @@ function marcadores(){
 function verificarVertical() {
     var uno=[], dos=[], tres=[], cuatro=[], cinco=[], seis=[], siete=[];
     for (var i = 0; i <= 6; i++) {
-      uno[i]=$(".col-1").find( "div" ).eq( i ).attr('class');
-      dos[i]=$(".col-2").find( "div" ).eq( i ).attr('class');
-      tres[i]=$(".col-3").find( "div" ).eq( i ).attr('class');
-      cuatro[i]=$(".col-4").find( "div" ).eq( i ).attr('class');
-      cinco[i]=$(".col-5").find( "div" ).eq( i ).attr('class');
-      seis[i]=$(".col-6").find( "div" ).eq( i ).attr('class');
-      siete[i]=$(".col-7").find( "div" ).eq( i ).attr('class');
+      uno[i]=$(".col-1").find( "img" ).eq( i ).attr('src');
+      dos[i]=$(".col-2").find( "img" ).eq( i ).attr('src');
+      tres[i]=$(".col-3").find( "img" ).eq( i ).attr('src');
+      cuatro[i]=$(".col-4").find( "img" ).eq( i ).attr('src');
+      cinco[i]=$(".col-5").find( "img" ).eq( i ).attr('src');
+      seis[i]=$(".col-6").find( "img" ).eq( i ).attr('src');
+      siete[i]=$(".col-7").find( "img" ).eq( i ).attr('src');
     };
+    for(var j=1;j<8;j++){
+  		matriz=matriz+$(".col-"+j).children().length;};
     for (var c = 0, d=1, e=2; e <=6; e++) {
 
-      if (uno[c]==uno[d]&&uno[d]==uno[e]) {
-        $(".col-1").find( "div" ).eq( c ).attr("class","borrar");
-        $(".col-1").find( "div" ).eq( d ).attr("class","borrar");
-        $(".col-1").find( "div" ).eq( e ).attr("class","borrar");
+      if (uno[c]==uno[d]&&uno[d]==uno[e]&&matriz==49) {
+        $(".col-1").find( "img" ).eq( c ).attr("class","borrar");
+        $(".col-1").find( "img" ).eq( d ).attr("class","borrar");
+        $(".col-1").find( "img" ).eq( e ).attr("class","borrar");
       };
-      if (dos[c]==dos[d]&&dos[d]==dos[e]) {
-        $(".col-2").find( "div" ).eq( c ).attr("class","borrar");
-        $(".col-2").find( "div" ).eq( d ).attr("class","borrar");
-        $(".col-2").find( "div" ).eq( e ).attr("class","borrar");
+      if (dos[c]==dos[d]&&dos[d]==dos[e]&&matriz==49) {
+        $(".col-2").find( "img" ).eq( c ).attr("class","borrar");
+        $(".col-2").find( "img" ).eq( d ).attr("class","borrar");
+        $(".col-2").find( "img" ).eq( e ).attr("class","borrar");
+        score=score+5;
       };
-      if (tres[c]==tres[d]&&tres[d]==tres[e]) {
-        $(".col-3").find( "div" ).eq( c ).attr("class","borrar");
-        $(".col-3").find( "div" ).eq( d ).attr("class","borrar");
-        $(".col-3").find( "div" ).eq( e ).attr("class","borrar");
+      if (tres[c]==tres[d]&&tres[d]==tres[e]&&matriz==49) {
+        $(".col-3").find( "img" ).eq( c ).attr("class","borrar");
+        $(".col-3").find( "img" ).eq( d ).attr("class","borrar");
+        $(".col-3").find( "img" ).eq( e ).attr("class","borrar");
+        score=score+5;
       };
-      if (cuatro[c]==cuatro[d]&&cuatro[d]==cuatro[e]) {
-        $(".col-4").find( "div" ).eq( c ).attr("class","borrar");
-        $(".col-4").find( "div" ).eq( d ).attr("class","borrar");
-        $(".col-4").find( "div" ).eq( e ).attr("class","borrar");
+      if (cuatro[c]==cuatro[d]&&cuatro[d]==cuatro[e]&&matriz==49) {
+        $(".col-4").find( "img" ).eq( c ).attr("class","borrar");
+        $(".col-4").find( "img" ).eq( d ).attr("class","borrar");
+        $(".col-4").find( "img" ).eq( e ).attr("class","borrar");
+        score=score+5;
       };
-      if (cinco[c]==cinco[d]&&cinco[d]==cinco[e]) {
-        $(".col-5").find( "div" ).eq( c ).attr("class","borrar");
-        $(".col-5").find( "div" ).eq( d ).attr("class","borrar");
-        $(".col-5").find( "div" ).eq( e ).attr("class","borrar");
+      if (cinco[c]==cinco[d]&&cinco[d]==cinco[e]&&matriz==49) {
+        $(".col-5").find( "img" ).eq( c ).attr("class","borrar");
+        $(".col-5").find( "img" ).eq( d ).attr("class","borrar");
+        $(".col-5").find( "img" ).eq( e ).attr("class","borrar");
+        score=score+5;
       };
-      if (seis[c]==seis[d]&&seis[d]==seis[e]) {
-        $(".col-6").find( "div" ).eq( c ).attr("class","borrar");
-        $(".col-6").find( "div" ).eq( d ).attr("class","borrar");
-        $(".col-6").find( "div" ).eq( e ).attr("class","borrar");
+      if (seis[c]==seis[d]&&seis[d]==seis[e]&&matriz==49) {
+        $(".col-6").find( "img" ).eq( c ).attr("class","borrar");
+        $(".col-6").find( "img" ).eq( d ).attr("class","borrar");
+        $(".col-6").find( "img" ).eq( e ).attr("class","borrar");
+        score=score+5;
       };
-      if (siete[c]==siete[d]&&siete[d]==siete[e]) {
-        $(".col-7").find( "div" ).eq( c ).attr("class","borrar");
-        $(".col-7").find( "div" ).eq( d ).attr("class","borrar");
-        $(".col-7").find( "div" ).eq( e ).attr("class","borrar");
+      if (siete[c]==siete[d]&&siete[d]==siete[e]&&matriz==49) {
+        $(".col-7").find( "img" ).eq( c ).attr("class","borrar");
+        $(".col-7").find( "img" ).eq( d ).attr("class","borrar");
+        $(".col-7").find( "img" ).eq( e ).attr("class","borrar");
+        score=score+5;
       };
       $(".borrar").hide("pulsate",1400,function(){
         $(".borrar").remove();
       });
       c++;
       d++;
+      $("#score-text").html(" "+score+" ");
     }
+    matriz=0;
 }
 
 //verificar si hay combinaciones en horizontal
 function verificarHorizontal() {
     var unoH=[], dosH=[], tresH=[], cuatroH=[], cincoH=[], seisH=[], sieteH=[];
     for (var iH = 0; iH <= 6; iH++) {
-      unoH[iH]=$(".col-1").find( "div" ).eq( iH ).attr('class');
-      dosH[iH]=$(".col-2").find( "div" ).eq( iH ).attr('class');
-      tresH[iH]=$(".col-3").find( "div" ).eq( iH ).attr('class');
-      cuatroH[iH]=$(".col-4").find( "div" ).eq( iH ).attr('class');
-      cincoH[iH]=$(".col-5").find( "div" ).eq( iH ).attr('class');
-      seisH[iH]=$(".col-6").find( "div" ).eq( iH ).attr('class');
-      sieteH[iH]=$(".col-7").find( "div" ).eq( iH ).attr('class');
+      unoH[iH]=$(".col-1").find( "img" ).eq( iH ).attr('src');
+      dosH[iH]=$(".col-2").find( "img" ).eq( iH ).attr('src');
+      tresH[iH]=$(".col-3").find( "img" ).eq( iH ).attr('src');
+      cuatroH[iH]=$(".col-4").find( "img" ).eq( iH ).attr('src');
+      cincoH[iH]=$(".col-5").find( "img" ).eq( iH ).attr('src');
+      seisH[iH]=$(".col-6").find( "img" ).eq( iH ).attr('src');
+      sieteH[iH]=$(".col-7").find( "img" ).eq( iH ).attr('src');
     };
+    for(var j=1;j<8;j++){
+      matriz=matriz+$(".col-"+j).children().length;};
     for (var cH = 0; cH <=6; cH++) {
 
-      if (unoH[cH]==dosH[cH]&&unoH[cH]==tresH[cH]) {
-        $(".col-1").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-2").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-3").find( "div" ).eq( cH ).attr("class","borrar2");
+      if (unoH[cH]==dosH[cH]&&unoH[cH]==tresH[cH]&&matriz==49) {
+        $(".col-1").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-2").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-3").find( "img" ).eq( cH ).attr("class","borrar2");
+        score=score+5;
       };
-      if (dosH[cH]==tresH[cH]&&dosH[cH]==cuatroH[cH]) {
-        $(".col-2").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-3").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-4").find( "div" ).eq( cH ).attr("class","borrar2");
+      if (dosH[cH]==tresH[cH]&&dosH[cH]==cuatroH[cH]&&matriz==49) {
+        $(".col-2").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-3").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-4").find( "img" ).eq( cH ).attr("class","borrar2");
+        score=score+5;
       };
-      if (tresH[cH]==cuatroH[cH]&&tresH[cH]==cincoH[cH]) {
-        $(".col-3").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-4").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-5").find( "div" ).eq( cH ).attr("class","borrar2");
+      if (tresH[cH]==cuatroH[cH]&&tresH[cH]==cincoH[cH]&&matriz==49) {
+        $(".col-3").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-4").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-5").find( "img" ).eq( cH ).attr("class","borrar2");
+        score=score+5;
       };
-      if (cuatroH[cH]==cincoH[cH]&&cuatroH[cH]==seisH[cH]) {
-        $(".col-4").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-5").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-6").find( "div" ).eq( cH ).attr("class","borrar2");
+      if (cuatroH[cH]==cincoH[cH]&&cuatroH[cH]==seisH[cH]&&matriz==49) {
+        $(".col-4").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-5").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-6").find( "img" ).eq( cH ).attr("class","borrar2");
+        score=score+5;
       };
-      if (cincoH[cH]==seisH[cH]&&cincoH[cH]==sieteH[cH]) {
-        $(".col-5").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-6").find( "div" ).eq( cH ).attr("class","borrar2");
-        $(".col-7").find( "div" ).eq( cH ).attr("class","borrar2");
+      if (cincoH[cH]==seisH[cH]&&cincoH[cH]==sieteH[cH]&&matriz==49) {
+        $(".col-5").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-6").find( "img" ).eq( cH ).attr("class","borrar2");
+        $(".col-7").find( "img" ).eq( cH ).attr("class","borrar2");
+        score=score+5;
       };
     }
     $(".borrar2").hide("pulsate",1400,function(){
       $(".borrar2").remove();
+      $("#score-text").html(" "+score+" ");
     });
+    matriz=0;
+};
+
+
+
+setInterval('eliminar()', 500);
+var mov=0;
+var matriz=0;
+function eliminar(){
+
+		$(".fruta").draggable({
+			disabled:false,
+			containment:".panel-tablero",
+			revert:true,
+			revertDuration:0,
+			snap:".fruta",
+			snapMode:"inner",
+			snapTolerance:40,
+			start:function(event,ui){
+				mov=mov+1;
+				$("#movimientos-text").html(mov);}
+		});
+
+	$(".fruta").droppable({
+		drop:function (event,ui){
+			var dropped=ui.draggable;
+			var droppedOn=this;
+			espera=0;
+			espera=dropped.swap($(droppedOn));
+      },
+	});
+};
+
+//---------Función para intercambiar dulces-------------------------------------
+jQuery.fn.swap=function(b){
+	b=jQuery(b)[0];
+	var a=this[0];
+	var t=a.parentNode.insertBefore(document.createTextNode(''),a);
+	b.parentNode.insertBefore(a,b);
+	t.parentNode.insertBefore(b,t);
+	t.parentNode.removeChild(t);
+	return this;
 };
